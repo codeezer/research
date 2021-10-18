faa5ccebcb89fb02a5848da5d7fa61bf115e12b0
Ramesh Neupane <ezerames@gmail.com>
2021-10-18 13:25:09 -0600

    uppaal api

--- uppaal-api/testAPI.java
+++ uppaal-api/testAPI.java
@@ -1,70 +1,13 @@
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
 
-import com.uppaal.engine.Engine;
+import com.uppaal.engine.CannotEvaluateException;
 import com.uppaal.engine.EngineException;
-import com.uppaal.engine.EngineStub;
-import com.uppaal.engine.Problem;
-import com.uppaal.model.core2.Document;
-import com.uppaal.model.core2.PrototypeDocument;
-import com.uppaal.model.system.UppaalSystem;
-import com.uppaal.model.system.Process;
 
-public class testAPI {
-	public static Document loadModel(String location) throws IOException
-    {
-		try {
-			// try URL scheme (useful to fetch from Internet):
-			return new PrototypeDocument().load(new URL(location));
-		} catch (MalformedURLException ex) {
-			// not URL, retry as it were a local filepath:
-			return new PrototypeDocument().load(new URL("file", null, location));
-		}
-    }
 
-    public static Engine connectToEngine() throws EngineException, IOException
-    {
-		String os = System.getProperty("os.name");
-		String here = System.getProperty("user.dir");
-		String path = null;
-		if ("Linux".equals(os)) {
-			path = here+"/bin-Linux/server";
-		} else {
-			path = here+"\\bin-Windows\\server.exe";
-		}
-		Engine engine = new Engine();
-		engine.setServerPath(path);
-		engine.setServerHost("localhost");
-		engine.setConnectionMode(EngineStub.BOTH);
-		engine.connect();
-		return engine;
-    }
+public class TestAPI {
 
-    public static UppaalSystem compile(Engine engine, Document doc)
-		throws EngineException, IOException
-    {
-		// compile the model into system:
-		ArrayList<Problem> problems = new ArrayList<Problem>();
-		UppaalSystem sys = engine.getSystem(doc, problems);
-		if (!problems.isEmpty()) {
-			boolean fatal = false;
-			System.out.println("There are problems with the document:");
-			for (Problem p : problems) {
-				System.out.println(p.toString());
-				if (!"warning".equals(p.getType())) { // ignore warnings
-					fatal = true;
-				}
-			}
-			if (fatal) {
-				System.exit(1);
-			}
-		}
-		return sys;
-    }
-
-	public static void main(String[] args) {
+        
+    public static void main(String[] args) {
         if (args.length<1) {
             System.out.println("This is a demo of Uppaal model.jar API");
             System.out.println("Use one of the following arguments:");
@@ -73,29 +16,44 @@ public class testAPI {
             System.out.println("  <path/file.xml>");
             System.exit(0);
         }
+        
         try {
-			Document doc = null;
-			// load model from a file/internet:
-			doc = loadModel(args[0]);
+        	String modelFile = args[0];
+        	UppaalAPI uppaalAPI = new UppaalAPI(modelFile);
 
-			// connect to the engine server:
-			Engine engine = connectToEngine();
+			// SymbolicState state = engine.getInitialState(system);
+			
+//        	String query1  =  "E<> intakePump1.running";
+//        	char queryStatus1 = uppaalAPI.getQueryStatus(query1, "will intakePump be running?"); 
+//        	System.out.println(query1 + ": " + queryStatus1);
+//        	
+//        	String query2  =  "E<> emergencyFlush.running";
+//        	char queryStatus2 = uppaalAPI.getQueryStatus(query2, "will emergencyFlush be running?"); 
+//        	System.out.println(query2 + ": " + queryStatus2);
+        	
+        	uppaalAPI.printCurrentState();
+        	uppaalAPI.symbolicSimulation(50);
+        	
+        	System.out.println("------------------------------------------------------------------------");
+        	
+        	uppaalAPI.updateVariableValue("EmergencyStop", 1);
+        	uppaalAPI.updateVariableValue("RunProcess", 0);
+        	uppaalAPI.updateVariableValue("attack_time", 2);
 
-            // create a link to a local Uppaal process:
-			UppaalSystem system = compile(engine, doc);
-
-			for (String variable : system.getVariables()) {
-				System.out.println(variable);
-			}
-
-
-			engine.disconnect();
+        	uppaalAPI.printCurrentState();
+        	
+			uppaalAPI.symbolicSimulation(50);
+			
+			uppaalAPI.endConnection();
+			
         } catch (EngineException ex) {
             ex.printStackTrace(System.err);
             System.exit(1);
         } catch (IOException ex) {
             ex.printStackTrace(System.err);
             System.exit(1);
-        }
+        } catch (CannotEvaluateException e) {
+			e.printStackTrace();
+		}
     }
 }
